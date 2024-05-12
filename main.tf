80d2e8f6e3fa653bc6243f632b36ed9531270032
ibrahimvd <ibrahim.sikandar@venturedive.com>
2024-05-12 22:27:33 +0500

    add create dynamodb table

--- main.tf
+++ main.tf
@@ -8,3 +8,16 @@ resource "aws_s3_bucket" "files_bucket" {
     Environment = "Test"
   }
 }
+
+########## CREATE DYNAMODB TABLE ##########
+
+resource "aws_dynamodb_table" "files_table" {
+  name           = "Files"
+  hash_key       = "FileName"
+  read_capacity  = 5
+  write_capacity = 5
+  attribute {
+    name = "FileName"
+    type = "S"
+  }
+}
