3a960e061d2e4a74f5999283e77448ddb4e47802
ibrahimvd <ibrahim.sikandar@venturedive.com>
2024-05-12 22:57:59 +0500

    fix provider file issue

--- provider.tf
+++ provider.tf
@@ -3,10 +3,10 @@ provider "aws" {
   secret_key = "foobar"
   region     = "eu-central-1"
 
-  skip_requesting_account_id  = true
   s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = false
 
   endpoints {
     apigateway     = "http://localhost:4566"
