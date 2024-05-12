77ca43b1e0992334a80aac27152c8c3c5ddb451e
ibrahimvd <ibrahim.sikandar@venturedive.com>
2024-05-12 22:50:57 +0500

    create lambda function to exe stepfunc

--- main.tf
+++ main.tf
@@ -106,3 +106,31 @@ EOF
 
 ########## CREATE LAMBDA FUNCTION ##########
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  filename      = "lambda.zip"
+  function_name = "lambda-stepfunction"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda.lambda_handler"
+
+  runtime = "python3.8"
+}
+
+########################################################################################
