ffcd64e027e438d990247d60bb1d8230c1b40e26
ibrahimvd <ibrahim.sikandar@venturedive.com>
2024-05-12 22:53:59 +0500

    create s3 event notification with policy

--- main.tf
+++ main.tf
@@ -152,3 +152,23 @@ resource "aws_iam_role_policy" "step_function_policy" {
     ]
   })
 }
+
+########## CREATE S3 EVENT NOTIFICATION with POLICY TO INVOKE LAMBDA ##########
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.files_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.files_bucket.bucket
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.my_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+}
